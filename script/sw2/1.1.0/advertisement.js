453d3587f507d3b443564f727caea327a795f9ff
Eric Schmit <eschmit@groceryshopping.net>
2014-08-07 15:36:21 -0500

    updated adblocker logic
    advertisement.js no longer writes a new tag, but simply declares a
    global var that we can check

--- script/sw2/1.1.0/advertisement.js
+++ script/sw2/1.1.0/advertisement.js
@@ -1,7 +1,2 @@
-//document.write('<div id="tester" style="display:none;">an advertisement</div>');
-var body = document.getElementsByTagName('body').item(0);
-var tag = document.createElement('div');
-tag.setAttribute('style', 'display:none;');
-tag.setAttribute('id', 'tester');
-body.appendChild(tag);
+var gsnglobaltester = {};
 
