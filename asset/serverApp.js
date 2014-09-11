ed194aa2a281e407e8695e7ca28ca15e7707d445
noogen <tnguyen@groceryshopping.net>
2014-09-10 21:44:22 -0500

    node serverApp.js work for both cdn and tfs.

--- asset/serverApp.js
+++ asset/serverApp.js
@@ -23,10 +23,9 @@ function startServer(chainId) {
     req.pipe(request({ uri: newUrl, method: req.method })).pipe(res);
   });
 
-  // app.use(bodyParser());
   app.use(methodOverride());
-  app.use('/app', express.static(servicePath + path.sep + 'app'));
-  app.use('/content', express.static(servicePath + path.sep + 'content'));
+  app.use('/asset', express.static(servicePath + path.sep + 'asset'));
+  app.use('/script', express.static(servicePath + path.sep + 'script'));
 
   // routes
   app.get('/no-javascript.html', function (request, response) {
@@ -49,7 +48,7 @@ function startServer(chainId) {
     fs.readFile(indexFile, 'utf8', function (err, str) {
       str = str.replace('@if (this.ViewBag.CanDebug == "true") {@Scripts.Render("~/gsncore")}', '')
       str = str.replace('@Gsn.Digital.Web.MvcApplication.ProxyMasterUrl', 'http://clientapix.gsn2.com/api/v1');
-      str = str.replace(/\@this.ViewBag.CdnUrl/gi, '//cdn-beta.gsngrocers.com');
+      str = str.replace(/\@this.ViewBag.CdnUrl/gi, '');
       str = str.replace('@this.ViewBag.FavIcon',  appPath  + '/' + chainId + '/images/favicon.ico');
       str = str.replace('@this.ViewBag.Title', chainId);
       str = str.replace('@RenderSection("htmlhead", false)', '<link href="' + appPath + '/' + chainId + '/styles/app.css" rel="stylesheet" />');
@@ -59,7 +58,7 @@ function startServer(chainId) {
   '} catch (e) { }\n' +
   '})(window.globalConfig || {});\n' +
   '</script>\n' +                                                                                                           
-  '<script src="/app/' + chainId + '/storeApp.js"></script>\n' +
+  '<script src="/asset/' + chainId + '/storeApp.js"></script>\n' +
   '<script src="http://clientapix.gsn2.com/api/v1/store/siteconfig/' + chainId + '?callback=globalConfigCallback"></script>\n' 
   // + '<script src="http://localhost:35729/livereload.js"></script>'
       );
