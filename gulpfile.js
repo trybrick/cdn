f82be19ee136b4842d71927e90e6e4591e83d934
noogen <tnguyen@groceryshopping.net>
2015-03-17 16:43:51 -0500

    Add bower install.

---
+++ gulpfile.js
@@ -0,0 +1,33 @@
+var gulp =       require('gulp');
+var gutil =      require('gulp-util');
+var uglify =     require('gulp-uglify');
+var jshint =     require('gulp-jshint');
+var webpack =    require('gulp-webpack');
+var rename =     require('gulp-rename');
+var coffee =     require('gulp-coffee');
+var concat =     require('gulp-concat');
+var clean =      require('gulp-clean');
+var header =     require('gulp-header');
+                 require('gulp-grunt')(gulp);
+
+var chains = [218];
+var tasks = [];
+
+for(var c in chains) {
+  var chain = chains[c];
+  gulp.task('ds-' + chain, function() {
+  return gulp.src('bower_components/ds-' + chain + '/dist/**', { base: 'bower_components/ds-' + chain + '/dist/' })
+    .pipe(gulp.dest('asset/' + chain));
+  });
+  
+  tasks.push('ds-' + chain);
+}
+gulp.task('default', tasks, function() {
+  console.log('success');
+});
+
+gulp.task('watch', function() {
+  gulp.watch('src/gsndfp.js', ['default']);
+});
+
+gulp.task('test', ['grunt-jasmine']);
