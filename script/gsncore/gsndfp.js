01b311cfa5b1322eb50fe9fabbfb4d692d64aec5
noogen <tnguyen@groceryshopping.net>
2015-03-19 15:29:25 -0500

    Add circplus.

--- script/gsncore/gsndfp.js
+++ script/gsncore/gsndfp.js
@@ -8,9 +8,365 @@
  */
 
 /*!
+ *  Project: circplus
+
+ Initialize circplus using the code below:
+
+to init, tag any div with class "circplus"
+  $.circPlus({dfpID: '6394/6394.test', setTargeting: {dept: 'beverages'} });
+  $.circPlus({ dfpID: '6394/partner-root-3566/123.giantcarlisle', setTargeting: {dept: 'beverages'} });
+
+same command to refresh:
+  $.circPlus({ dfpID: '6394/partner-root-3566/123.giantcarlisle', setTargeting: {dept: 'beverages'} });
+ */
+(function($, window) {
+  'use strict';
+  var $adCollection, bodyTemplate, count, createAds, dfpBlocked, dfpID, dfpIsLoaded, dfpLoader, dfpOptions, dfpScript, dfpSelector, displayAds, getDimensions, getID, init, isInView, rendered, sessionStorageX, setOptions, storeAs;
+  sessionStorageX = sessionStorage;
+  if (typeof sessionStorageX === 'undefined') {
+    sessionStorageX = {
+      getItem: function() {},
+      setItem: function() {}
+    };
+  }
+  dfpScript = this;
+  dfpID = '';
+  count = 0;
+  rendered = 0;
+  dfpSelector = '.circplus';
+  dfpOptions = {};
+  dfpIsLoaded = false;
+  $adCollection = void 0;
+  storeAs = 'circplus';
+  bodyTemplate = '<div class="gsn-slot-container"><div class="cpslot cpslot2" data-companion="true" data-dimensions="300x50"></div></div><div class="gsn-slot-container"><div class="cpslot cpslot1" data-dimensions="300x100,300x120"></div></div>';
+  init = function(id, selector, options) {
+    dfpID = id;
+    if ($(selector).html() === '') {
+      if (options.templateSelector) {
+        $(selector).html($(options.templateSelector).html());
+      } else {
+        $(selector).html(options.bodyTemplate || bodyTemplate);
+      }
+    }
+    $adCollection = $($('.cpslot').get().reverse());
+    dfpLoader();
+    setOptions(options);
+    $(function() {
+      createAds();
+      displayAds();
+    });
+  };
+  setOptions = function(options) {
+    dfpOptions = {
+      setTargeting: {},
+      setCategoryExclusion: '',
+      setLocation: '',
+      enableSingleRequest: true,
+      collapseEmptyDivs: true,
+      refreshExisting: true,
+      disablePublisherConsole: false,
+      disableInitialLoad: false,
+      inViewOnly: false,
+      noFetch: false
+    };
+    $.extend(true, dfpOptions, options);
+    if (dfpOptions.googletag) {
+      window.googletag.cmd.push(function() {
+        $.extend(true, window.googletag, dfpOptions.googletag);
+      });
+    }
+  };
+  createAds = function() {
+    $adCollection.each(function() {
+      var $adUnit, $existingContent, adUnitID, dimensions;
+      $adUnit = $(this);
+      count++;
+      adUnitID = getID($adUnit, 'gsncircplus', count);
+      dimensions = getDimensions($adUnit);
+      $existingContent = $adUnit.html();
+      $adUnit.html('').addClass('display-none');
+      window.googletag.cmd.push(function() {
+        var $adUnitData, companion, exclusions, exclusionsGroup, googleAdUnit, targeting, valueTrimmed;
+        googleAdUnit = void 0;
+        $adUnitData = $adUnit.data(storeAs);
+        if ($adUnitData) {
+          return;
+        }
+        dfpID = dfpID.replace(/(\/\/)+/gi, '/').replace(/\s+/gi, '').replace(/(\/)$/, '/');
+        if (dfpID.indexOf('/') !== 0) {
+          dfpID = '/' + dfpID;
+        }
+        if ($adUnit.data('outofpage')) {
+          googleAdUnit = window.googletag.defineOutOfPageSlot(dfpID, adUnitID).addService(window.googletag.pubads());
+        } else {
+          googleAdUnit = window.googletag.defineSlot(dfpID, dimensions, adUnitID).addService(window.googletag.pubads());
+        }
+        companion = $adUnit.data('companion');
+        if (companion) {
+          googleAdUnit.addService(window.googletag.companionAds());
+        }
+        targeting = $adUnit.data('targeting');
+        if (targeting) {
+          if (typeof targeting === 'string') {
+            targeting = eval('(' + targeting + ')');
+          }
+          $.each(targeting, function(k, v) {
+            if (k === 'brand') {
+              sessionStorageX.setItem('brand', v);
+            }
+            googleAdUnit.setTargeting(k, v);
+          });
+        }
+        exclusions = $adUnit.data('exclusions');
+        if (exclusions) {
+          exclusionsGroup = exclusions.split(',');
+          valueTrimmed = void 0;
+          $.each(exclusionsGroup, function(k, v) {
+            valueTrimmed = $.trim(v);
+            if (valueTrimmed.length > 0) {
+              googleAdUnit.setCategoryExclusion(valueTrimmed);
+            }
+          });
+        }
+        googleAdUnit.oldRenderEnded = googleAdUnit.oldRenderEnded || googleAdUnit.renderEnded;
+        googleAdUnit.renderEnded = function() {
+          var display;
+          rendered++;
+          display = $adUnit.css('display');
+          $adUnit.removeClass('display-none').addClass('display-' + display);
+          googleAdUnit.oldRenderEnded();
+          if (typeof dfpOptions.afterEachAdLoaded === 'function') {
+            dfpOptions.afterEachAdLoaded.call(this, $adUnit);
+          }
+          if (typeof dfpOptions.afterAllAdsLoaded === 'function' && rendered === count) {
+            dfpOptions.afterAllAdsLoaded.call(this, $adCollection);
+          }
+        };
+        $adUnit.data(storeAs, googleAdUnit);
+      });
+    });
+    window.googletag.cmd.push(function() {
+      var exclusionsGroup, valueTrimmed;
+      if (typeof dfpOptions.setTargeting['brand'] === 'undefined') {
+        if (sessionStorageX.getItem('brand')) {
+          dfpOptions.setTargeting['brand'] = sessionStorageX.getItem('brand');
+        }
+      }
+      if (dfpOptions.enableSingleRequest === true) {
+        window.googletag.pubads().enableSingleRequest();
+      }
+      $.each(dfpOptions.setTargeting, function(k, v) {
+        if (k === 'brand') {
+          sessionStorageX.setItem('brand', v);
+        }
+        window.googletag.pubads().setTargeting(k, v);
+      });
+      if (typeof dfpOptions.setLocation === 'object') {
+        if (typeof dfpOptions.setLocation.latitude === 'number' && typeof dfpOptions.setLocation.longitude === 'number' && typeof dfpOptions.setLocation.precision === 'number') {
+          window.googletag.pubads().setLocation(dfpOptions.setLocation.latitude, dfpOptions.setLocation.longitude, dfpOptions.setLocation.precision);
+        } else if (typeof dfpOptions.setLocation.latitude === 'number' && typeof dfpOptions.setLocation.longitude === 'number') {
+          window.googletag.pubads().setLocation(dfpOptions.setLocation.latitude, dfpOptions.setLocation.longitude);
+        }
+      }
+      if (dfpOptions.setCategoryExclusion.length > 0) {
+        exclusionsGroup = dfpOptions.setCategoryExclusion.split(',');
+        valueTrimmed = void 0;
+        $.each(exclusionsGroup, function(k, v) {
+          valueTrimmed = $.trim(v);
+          if (valueTrimmed.length > 0) {
+            window.googletag.pubads().setCategoryExclusion(valueTrimmed);
+          }
+        });
+      }
+      if (dfpOptions.collapseEmptyDivs === true || dfpOptions.collapseEmptyDivs === 'original') {
+        window.googletag.pubads().collapseEmptyDivs();
+      }
+      if (dfpOptions.disablePublisherConsole === true) {
+        window.googletag.pubads().disablePublisherConsole();
+      }
+      if (dfpOptions.disableInitialLoad === true) {
+        window.googletag.pubads().disableInitialLoad();
+      }
+      if (dfpOptions.noFetch === true) {
+        window.googletag.pubads().noFetch();
+      }
+      window.googletag.companionAds().setRefreshUnfilledSlots(true);
+      window.googletag.enableServices();
+    });
+  };
+  isInView = function(elem) {
+    var docViewBottom, docViewTop, elemBottom, elemTop;
+    docViewTop = $(window).scrollTop();
+    docViewBottom = docViewTop + $(window).height();
+    elemTop = elem.offset().top;
+    elemBottom = elemTop + elem.height();
+    return elemTop + (elemBottom - elemTop) / 2 >= docViewTop && elemTop + (elemBottom - elemTop) / 2 <= docViewBottom;
+  };
+  displayAds = function() {
+    var toPush;
+    toPush = [];
+    $adCollection.each(function() {
+      var $adUnit, $adUnitData;
+      $adUnit = $(this);
+      $adUnitData = $adUnit.data(storeAs);
+      if (dfpOptions.refreshExisting && $adUnitData && $adUnit.data('gsnDfpExisting')) {
+        if (!dfpOptions.inViewOnly || isInView($adUnit) && $adUnit.is(':visible')) {
+          toPush.push($adUnitData);
+        }
+      } else {
+        $adUnit.data('gsnDfpExisting', true);
+        window.googletag.cmd.push(function() {
+          window.googletag.display($adUnit.attr('id'));
+        });
+      }
+    });
+    if (toPush.length > 0) {
+      window.googletag.cmd.push(function() {
+        window.googletag.pubads().refresh(toPush);
+      });
+    }
+  };
+  getID = function($adUnit, adUnitName, count) {
+    if (!dfpOptions.refreshExisting) {
+      $adUnit.data(storeAs, null);
+      $adUnit.data('gsnDfpExisting', null);
+      if ($adUnit.attr('id')) {
+        $adUnit.attr('id', adUnitName + '-auto-gen-id-' + count);
+      }
+    }
+    return $adUnit.attr('id') || $adUnit.attr('id', adUnitName + '-auto-gen-id-' + count).attr('id');
+  };
+  getDimensions = function($adUnit) {
+    var dimensionGroups, dimensions, dimensionsData;
+    dimensions = [];
+    dimensionsData = $adUnit.data('dimensions');
+    if (dimensionsData) {
+      dimensionGroups = dimensionsData.split(',');
+      $.each(dimensionGroups, function(k, v) {
+        var dimensionSet;
+        dimensionSet = v.split('x');
+        dimensions.push([parseInt(dimensionSet[0], 10), parseInt(dimensionSet[1], 10)]);
+      });
+    } else {
+      dimensions.push([$adUnit.width(), $adUnit.height()]);
+    }
+    return dimensions;
+  };
+  dfpLoader = function() {
+    var gads, node, useSsl;
+    dfpIsLoaded = dfpIsLoaded || $('script[src*="googletagservices.com/tag/js/gpt.js"]').length;
+    if (dfpIsLoaded) {
+      return;
+    }
+    window.googletag = window.googletag || {};
+    window.googletag.cmd = window.googletag.cmd || [];
+    gads = document.createElement('script');
+    gads.async = true;
+    gads.type = 'text/javascript';
+    gads.onerror = function() {
+      dfpBlocked();
+    };
+    useSsl = 'https:' === document.location.protocol;
+    gads.src = (useSsl ? 'https:' : 'http:') + '//www.googletagservices.com/tag/js/gpt.js';
+    node = document.getElementsByTagName('script')[0];
+    node.parentNode.insertBefore(gads, node);
+    if (gads.style.display === 'none') {
+      dfpBlocked();
+    }
+  };
+  dfpBlocked = function() {
+    var commands;
+    commands = window.googletag.cmd;
+    setTimeout((function() {
+      var _defineSlot;
+      _defineSlot = function(name, dimensions, id, oop) {
+        window.googletag.ads.push(id);
+        window.googletag.ads[id] = {
+          renderEnded: function() {},
+          addService: function() {
+            return this;
+          }
+        };
+        return window.googletag.ads[id];
+      };
+      window.googletag = {
+        cmd: {
+          push: function(callback) {
+            callback.call(dfpScript);
+          }
+        },
+        ads: [],
+        pubads: function() {
+          return this;
+        },
+        noFetch: function() {
+          return this;
+        },
+        disableInitialLoad: function() {
+          return this;
+        },
+        disablePublisherConsole: function() {
+          return this;
+        },
+        enableSingleRequest: function() {
+          return this;
+        },
+        setTargeting: function() {
+          return this;
+        },
+        collapseEmptyDivs: function() {
+          return this;
+        },
+        enableServices: function() {
+          return this;
+        },
+        defineSlot: function(name, dimensions, id) {
+          return _defineSlot(name, dimensions, id, false);
+        },
+        defineOutOfPageSlot: function(name, id) {
+          return _defineSlot(name, [], id, true);
+        },
+        display: function(id) {
+          window.googletag.ads[id].renderEnded.call(dfpScript);
+          return this;
+        }
+      };
+      $.each(commands, function(k, v) {
+        window.googletag.cmd.push(v);
+      });
+    }), 50);
+  };
+
+  /**
+   * Add function to the jQuery / Zepto / tire namespace
+   * @param  String id      (Optional) The DFP account ID
+   * @param  Object options (Optional) Custom options to apply
+   */
+  $.circPlus = $.fn.circPlus = function(id, options) {
+    var selector;
+    options = options || {};
+    if (id === void 0) {
+      id = dfpID;
+    }
+    if (typeof id === 'object') {
+      options = id;
+      id = options.dfpID || dfpID;
+    }
+    selector = this;
+    if (typeof this === 'function') {
+      selector = dfpSelector;
+    }
+    init(id, selector, options);
+    return this;
+  };
+})(window.jQuery || window.Zepto || window.tire || window.$, window);
+
+
+/*!
  *  Project: gsnevent triggering
  * ===============================
  */
+
 
 /* Usage:
  *   For Publisher: 
@@ -21,6 +377,7 @@
 #
  * The following events are currently available: clickProduct, clickPromotion, clickBrand, clickBrickOffer, clickRecipe, and clickLink
  */
+
 (function($, oldGsn, win, doc, gsnContext) {
   var Plugin, buildQueryString, createFrame, lastRefreshTime, myGsn, myParent$, myPlugin, oldGsnAdvertising, parent$, sessionStorageX, tickerFrame;
   sessionStorageX = win.sessionStorage;
