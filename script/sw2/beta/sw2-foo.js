6d2eb12c45a76efdfe08cf54befd8d86485186cb
noogen <tom@noogen.net>
2016-06-11 18:32:44 -0500

    remove old files

--- script/sw2/beta/sw2-foo.js
+++
@@ -1,9 +0,0 @@
-window.foo = function(data){
-	console.log('foo data ' + data);
-	var element = document.getElementById(data);
-	element.childNodes[1].contentWindow.postMessage('sw-close', '*');
-}
-
-window.onmessage = function(evt){
-	console.log('in outer, from inner :' + evt.data);
-}
